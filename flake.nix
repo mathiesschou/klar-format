e0f1eca5829c92277c50b4a2e645fe56995db093
mathiesschou <mathiesschou@icloud.com>
2026-03-12 11:47:15 +0100

    direnv for project

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOs/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDeafultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            cargo
+            rustc
+            rust-analyzer
+            rusfmt
+            clippy
+          ];
+        };
+      });
+}
